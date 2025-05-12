cb93eb574e7be8cdd7fe8b98f3817ca81554806e
yashvalid <patilllyashhh@gmail.com>
2025-05-12 13:01:54 +0530

    few changes

--- controllers/school.controller.js
+++ controllers/school.controller.js
@@ -34,6 +34,8 @@ module.exports.listSchools = async (req, res) => {
         ORDER BY distance ASC
     `, [userLatitude, userLongitude, userLatitude]);
 
+        if(!rows || rows.length === 0)
+            return res.status(404).json({ message: 'No schools found' });
         return res.status(200).json(rows);
     } catch(err){
         console.error('Error in /listSchools route:', err);
